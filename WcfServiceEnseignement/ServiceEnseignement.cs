d489b5d1aadd0816fafb3e9dcdfd9e84831381ab
El Bekri <elbekrizineb97@gmail.com>
2020-12-13 15:52:29 +0100

    Ajoutez des fichiers projet.

---
+++ WcfServiceEnseignement/ServiceEnseignement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceEnseignement
+{
+  
+    public class ServiceEnseignement : IServiceEnseignement
+    {
+        DataClassesEnseignementDataContext l = new DataClassesEnseignementDataContext();
+        public List<Eleve> getByCNE(string s)
+        {
+            return (from p in l.Eleve where p.CNE == s select p).ToList();
+        }
+    }
+}
